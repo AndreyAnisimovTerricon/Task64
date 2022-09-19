894fb919c33b1a8bc347f2932c942473f097a9ac
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-09-19 20:28:02 +0300

    Числа в промежутке от m до n

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n ");
+int n = Convert.ToInt32(Console.ReadLine());
+string NumbersRec(int m,int n)
+{
+    if (m <= n) return $"{m} "+ NumbersRec(m+1,n);
+    else return string.Empty;
+}
+
+Console.WriteLine(NumbersRec(m, n));
